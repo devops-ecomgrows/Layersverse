be630b554914e05f4bd5b60dde428113556c6dd0
martindev2811 <manhdao.dev@ecomgrows.com>
2022-02-14 14:04:31 +0700

    fix: trigger commit

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Signin() {
-  return <div>This is administration signin page test.</div>;
+  return <div>This is administration signin page test</div>;
 }
 
 export default Signin;
